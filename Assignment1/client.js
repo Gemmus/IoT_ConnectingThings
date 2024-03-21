b2ff6b4d9a1829f47e9fc7d78da8484bae4c18de
Gemmus <112064697+Gemmus@users.noreply.github.com>
2024-03-21 11:32:30 +0200

    Update client.js

--- Assignment1/client.js
+++ Assignment1/client.js
@@ -2,7 +2,7 @@ document.getElementById('guessForm').addEventListener('submit', function(event) 
     event.preventDefault(); // Prevent the default form submission
 
     const name = document.getElementById('getName').value;
-    const number = parseInt(document.getElementById('getNumber').value);
+    const number = document.getElementById('getNumber').value;
 
     window.location.href = `http://localhost:3000/${name}?num=${number}`;
 });
