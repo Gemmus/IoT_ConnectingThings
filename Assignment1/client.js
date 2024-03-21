149cf5471bb6a0275dfa097d19cf03fb5b7d3e68
Gemmus <112064697+Gemmus@users.noreply.github.com>
2024-03-21 09:48:38 +0200

    Update client.js

--- Assignment1/client.js
+++ Assignment1/client.js
@@ -4,10 +4,5 @@ document.getElementById('guessForm').addEventListener('submit', function(event) 
     const name = document.getElementById('getName').value;
     const number = parseInt(document.getElementById('getNumber').value);
 
-    // Validate number & send request to server
-    if (number < 1 || number > 10) {
-        window.location.href = `http://localhost:3000/name?fname=${name}+&number=${number}`;
-    } else {
-        window.location.href = `http://localhost:3000/${name}?num=${number}`;
-    }
+    window.location.href = `http://localhost:3000/${name}?num=${number}`;
 });
